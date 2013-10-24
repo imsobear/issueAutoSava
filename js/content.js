7e59284f2643bf3ffc635aa3fff8591ac443a0aa
大果 <liuxiong.lx@alibaba-inc.com>
2013-10-24 14:13:30 +0800

    以最简单 and 有点丑陋的方式实现需求

--- js/content.js
+++ js/content.js
@@ -1,56 +1,20 @@
 window.onload = function() {
     var doc = document,
-    layer = doc.createElement('div'),
-    p = doc.createElement('p'),
-    staySpan = doc.createElement('span'),
-    leaveSpan = doc.createElement('span');
-
-    p.innerHTML = '您正在编辑的 issue 还没有提交，确定要离开？';
-    staySpan.setAttribute('class', 'layer-stay');
-    leaveSpan.setAttribute('class', 'layer-leave');
-    staySpan.innerHTML = '继续编辑';
-    leaveSpan.innerHTML = '离开页面';
-
-    layer.classList.add('leave-layer', 'layer-hide');
-    layer.appendChild(p);
-    layer.appendChild(leaveSpan);
-    layer.appendChild(staySpan);
-
-    doc.body.appendChild(layer);
+        isSubmit = false;
 
     window.addEventListener('beforeunload', function (e) {
-        // e.preventDefault();
-        // layer.classList.remove('layer-hide');
-        // layer.style.display = 'block';
-
         // 输入框focus
         // 可提交issue
         // 可提交评论
-        var isFocused = doc.querySelector('#write_bucket_').classList.contains('focused'),
+        var target = e.target,
+            isFocused = doc.querySelector('#write_bucket_').classList.contains('focused'),
             isSubmit = !doc.querySelector('.button.primary:disabled'),
             idSubmitComment = doc.querySelector('.js-comment-and-button'),
             isSth = !!doc.querySelector('#issue_body').value;
 
+        // 点击的是提交按钮时就不弹窗
         if (isFocused || isSubmit || idSubmitComment || isSth) {
-            
             return '您正在编辑的 issue 还没有提交，确定要离开？';
-
         }
     });
-
-
-    doc.addEventListener('click', function(e) {
-        var target = e.target,
-        layer = doc.getElementsByClassName('leave-layer')[0];
-
-        if (target.classList.contains('layer-stay')) {
-            //点击了继续编辑的按钮
-            layer.classList.add('layer-hide');
-        } else if (target.classList.contains('layer-leave')) {
-            //点击了离开页面的按钮
-        } else {
-            return;
-        }
-    }, false);
-
 };
